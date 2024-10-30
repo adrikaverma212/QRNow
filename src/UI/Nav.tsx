1dc7fc24938881b17501adce6133d9665ecdd620
RiteshDev99 <riteshchaudhary5191@gmail.com>
2024-10-30 20:58:07 +0530

    added some feature

--- src/UI/Nav.tsx
+++ src/UI/Nav.tsx
@@ -1,63 +1,31 @@
 
-// import { useState } from 'react';
-// import { FaBars, FaUser, FaCog, FaQuestionCircle, FaSignOutAlt, FaSignInAlt, FaUserPlus, FaBell} from 'react-icons/fa';
-
-//   function Nav() {
-//     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  
-//     const toggleSidebar = () => {
-//       setIsSidebarOpen(!isSidebarOpen);
-//     };
-//   return (
-//     <div>
-//        <header className="header">
-//         <FaBars className="menu-icon" onClick={toggleSidebar} />
-//         <h1>PURVANCHAL CAFE</h1>
-//         <FaBell className="header-icon" />
-//       </header>
-
-//       <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-//         <ul>
-//           <li><FaUser /> Profile</li>
-//           <li><FaCog /> Settings</li>
-//           <li><FaQuestionCircle /> Help</li>
-//           <li><FaSignOutAlt /> Log Out</li>
-//           <li><FaSignInAlt /> Login</li>
-//           <li><FaUserPlus /> Sign Up</li>
-//         </ul>
-//       </div>
-     
-//     </div>
-   
-//   )
-// }
-
-// export default Nav
-
-
-
 
 import { useState } from 'react';
 import { FaBars, FaUser, FaCog, FaQuestionCircle, FaSignOutAlt, FaSignInAlt, FaUserPlus, FaBell } from 'react-icons/fa';
 
 function Nav() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false );
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
 
   const toggleSidebar = () => {
-    setIsSidebarOpen(prev => !prev);
+    setIsSidebarOpen(prevState => !prevState);
   };
 
   return (
     <div className="flex flex-col">
-   
+
       <header className="fixed top-0 left-0 w-full flex items-center bg-white p-4 shadow-lg z-50">
-        <FaBars className="menu-icon text-green-500 text-2xl cursor-pointer mr-4" onClick={toggleSidebar}  />
+        <FaBars
+          className="menu-icon text-green-500 text-2xl cursor-pointer mr-4"
+          onClick={toggleSidebar}
+        />
         <h1 className="flex-1 text-green-800 font-bold ml-4 font-DynaPuff">PURVANCHAL CAFE</h1>
         <FaBell className="header-icon text-green-500 text-xl cursor-pointer ml-4" />
       </header>
 
-    
-      <div className={`fixed top-0 h-full bg-[#F0F4F3] p-4 transition-transform duration-300 ease-in-out shadow-lg transform `}>
+      <div
+        className={`fixed top-0 left-0 h-full bg-[#F0F4F3] p-4 transition-transform duration-300 ease-in-out shadow-lg transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+          } w-64 z-40`}
+      >
         <ul className="mt-16 space-y-4">
           <li className="flex items-center p-4 text-green-500 text-lg cursor-pointer hover:bg-green-100">
             <FaUser className="mr-2" /> Profile
@@ -80,12 +48,14 @@ function Nav() {
         </ul>
       </div>
 
-      
-      <main className={`transition-all duration-300 ease-in-out ${isSidebarOpen ? 'ml-64' : 'ml-0'} mt-16 p-4`}>
-      
+      <main
+        className={`transition-all duration-300 ease-in-out ${isSidebarOpen ? 'ml-64' : 'ml-0'} mt-16 p-4`}
+      >
       </main>
     </div>
   );
 }
 
 export default Nav;
+
+
